e16c41e7859410a2f27fcf4416e32f1ecee7afee
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-10-10 16:53:52 +0200

    [add] js

--- js/script.js
+++ js/script.js
@@ -7,12 +7,20 @@ const userName = prompt('Scrivi il tuo nome');
 const userLastName = prompt('Scrivi il tuo cognome')
 const favoutiteColor = prompt('Scrivi il tuo colore prederito')
 const yearCurrent = 22;
+let newPassword;
+
+console.log('Nome --->', userName);
+console.log('Cognome --->', userLastName);
+console.log('Colore preferito --->', favoutiteColor);
 
 
 // metodo concatenato
 // const newPassword = userName + userLastName + favoutiteColor + yearCurrent;
 
-// metodo con template literals 
-const newPassword = `${userName}${userLastName}${favoutiteColor}${yearCurrent}`;
+// metodo con template literals + html e classe bootstrap
+newPassword = `
+  <span class= "text-uppercase text-primary"> la nuova password è:</span>
+  ${userName}${userLastName}${favoutiteColor}${yearCurrent}
+`;
 
-document.getElementById('new-password').innerHTML += newPassword;
+document.getElementById('new-password').innerHTML = newPassword;
